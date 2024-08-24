a286c1adacb62dbaaf0cb0f74b8f4f30e9eaa763
Ubergizmo <ubergizmogaming@gmail.com>
2024-08-24 19:47:17 +0200

    ## Task 1: Functional Component
    Component Greeting qui retourne "Hello, [name]!"

---
+++ react/test_interview_frontend/src/components/Greeting.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+//Component Greeting qui retourne "Hello, [name]!"
+
+function Greeting({ name }) {
+  return <h1>Hello, {name}!</h1>;
+}
+
+export default Greeting;
